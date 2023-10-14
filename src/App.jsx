e21ebb7cecdbe30572417501ecd34256a09b840f
rhutiksahu12 <shubhamsahu938@gmail.com>
2023-10-14 15:14:22 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Input from './components/Input'
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='h-screen'>
       <NavBar />
       <Input />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full flex py-6 justify-between items-center'>
+      <ul className='flex text-white'>
+        <li className='px-2'>git</li>
+        <li className='px-2'>lin</li>
+        <li className='px-2'>mail</li>
+        <li className='px-2'>insta</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
